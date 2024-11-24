364867b513f1ff1cc7851ec47a205ff0ef4aa7d3
劉小余 <paiyuliu@gmail.com>
2024-11-24 21:53:40 +0800

    修正修改檔名

--- changePhotoSize.py
+++ changePhotoSize.py
@@ -1,5 +1,5 @@
 import glob
-from moviepy.editor import *
+#from moviepy.editor import *
 from PIL import Image
 
 # read phto file
@@ -9,6 +9,7 @@ image_files = glob.glob("./Images/*.jpg")
 # resize photo to 2992 * 2000 and rename image file
 for i in range(len(image_files)):
     im = Image.open(image_files[i])
-    im_resized = im.resize((2992, 2000))
+    #im_resized = im.resize((2992, 2000))
+    im_resized = im.resize((1440, 960))
     file_name = image_files[i].split("\\")[-1]
     im_resized.save(f"./Images/RESIZE_{file_name}")
--- renameImageFileWithMd5.py
+++ renameImageFileWithMd5.py
@@ -2,6 +2,8 @@
 import os
 import hashlib
 import glob
+import datetime
+import exifread
 
 def get_md5(file_path):
     with open(file_path, 'rb') as f:
@@ -13,15 +15,42 @@ def get_md5(file_path):
             md5.update(data)
     return md5.hexdigest()
 
+def get_photo_taken_time(photo_path): 
+    with open(photo_path, 'rb') as photo: 
+        tags = exifread.process_file(photo) 
+        taken_time = tags.get('EXIF DateTimeOriginal') 
+
+        if taken_time is None:
+            # Get the creation time of the image file
+            creation_time = os.path.getctime(photo_path)
+            formatted_time = datetime.datetime.fromtimestamp(creation_time).strftime('%Y%m%d%H%M')
+            return formatted_time
+        # format yyyyMMddHHmm 
+        taken_time = str(taken_time).replace(":", "").replace(" ", "")
+        return taken_time
 
 # read phto file
 #image_files = ["GLU_4826.jpg","GLU_4828.jpg"]
-image_files = glob.glob("./Images/BOY*.JPG")
+image_files = glob.glob("./Images/*.JPG")
 
 # according the photo content to rename image file with md5
 for i in range(len(image_files)):
+    # Get the original file name without path and extension 
+    file_name = image_files[i].split("\\")[-1]
+    file_name = file_name.split(".")[0]
+
+    # continue for next file
+    # continue
+
+
+    # Get the md5 of the image file
     md5 = get_md5(image_files[i])
-    fileName = "BOY_"+md5 + ".jpg" 
+
+    # 抓取照片拍攝時間 (EXIF)
+    formatted_time = get_photo_taken_time(image_files[i])
+
+    # Create the new file name
+    fileName = file_name + "_" + formatted_time+"_"+ md5 + ".JPG"
     os.rename(image_files[i], fileName)
     print(image_files[i] + " -> " + fileName)
     #image_files[i] = fileName
